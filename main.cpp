62117030cfe81d5f7f91fabfe616ec37381ec340
Steve Hearnden <steve@hearnden.org.uk>
2021-03-28 14:09:05 +0100

    Fill out section tracking
    Ensure we can track sections as they change,
    make the sections capture their lines

--- main.cpp
+++ main.cpp
@@ -2,29 +2,115 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
 #include "json_lite.h"
 
+//
+// [stuff] # starts a new filter.
+// filter gets more restrictive as extra
+// classes are sequentially added.
+// if a filter is added in the same class as an active filter
+// then that disables the earlier filter, and adds the current filter.
+//
+// [all]  disables all filters
+// [none] selects nothing - items within [none] match nothing.
+//
+
+//////////////////////////////////////////////////
+// Filter
+// models the line which enables a filter.
+// full line is mLine
+// mClass is the class which this filter belongs to.
+// mKey is the value left of the equals sign e.g. gpio4
+// mValue is the value after the equals sign e.g. 1
+// mLine is the line which was parsed to get this data.
 class Filter
 {
 private:
   std::string mClass;
+  std::string mKey;
   std::string mValue;
   std::string mLine;
+  bool mEmpty;
 public:
-  Filter( const char * fltClass, const char * value, const char * line )
+  Filter()
+    : mEmpty( true )
+  {}
+  Filter( const char * fltClass, const char * key
+	  , const char * value, const char * line )
     : mClass( fltClass )
+    , mKey( key )
     , mValue( value )
     , mLine( line )
+    , mEmpty( false )
   {
   }
+  static bool parseFilter( const std::string & line, std::string &key,
+		      std::string &value)
+  {
+    // [gpio3=1] key=gpio3  value=1
+    // [0x01243] key=0x1243
+    // [HDMI:0]  key=HDMI:0
+    if( line.length() == 0 || line[0] != '[' ){
+      return false;
+    }
+    std::string _key;
+    std::string _value;
+    enum inputMode {imKey, imValue };
+    inputMode mode = imKey;
+    std::string::size_type pos = 1;
+    while( pos < line.length() ){
+      if( line[pos] == ']' ){
+	if( _key.length()> 0 &&
+	    (mode == imKey || _value.length() > 0 )){
+	  key = _key;
+	  value = _value;
+	  return true;
+	} else {
+	  return false;
+	}
+      } else if( line[pos] == '=' ){
+	if( mode == imValue ){
+	  return false;
+	}
+	mode = imValue;
+      } else {
+	if( mode == imKey ){
+	  _key += line[pos];
+	} else {
+	  _value += line[pos];
+	}
+      }
+      pos++;
+    }
+    return false;
+  }
 };
+
+class ConfigSetup;
+///////////////////////////////////////////////////////////////////
+// section - created each time the filter changes.
+// describes the lines with a specific filter-set added.
+// mSelection - the filters which are active.
+// mEntryFilter - the filter which started this section.  (May be empty for first section)
+// mLines - the lines in the section
 class Section
 {
+public:
   std::vector< Filter > mSelection;  // which filters are active...
   Filter mEntryFilter;
   std::vector< std::string > mLines;
+  void sectionChange( const std::string & line, const ConfigSetup & config );
 };
 
+class WholeFile
+{
+public:
+  std::vector<Section> mSections;
+};
+
+
+  
 //////////////////////////////////////////////////////////////
 // sigh - after reading json spec.  All "strings" are quoted with "
 //
@@ -36,7 +122,7 @@ const char * config =
   "super" :     [ "all", "none" ],
   "display" :   [ "edid" ],
   "cpuserial" : [ "0x%x" ], 
-  "HDMI" :      [ "hdmi:0", "hdmi:1" ],
+  "HDMI" :      [ "HDMI:0", "HDMI:1" ],
   "gpio" :      [ "gpio%d" ] 
   })##config";
 class Description
@@ -75,6 +161,27 @@ public:
   {}
   ConfigValue()
   {}
+  bool isValid( const std::string & key )
+  {
+    if( mBase == mKey ) return true;
+    if( key.substr( 0,mBase.length() ) != mBase ){
+      return false;
+    }
+    if( mParam.length() == 0 ) return false;
+    if( mParam == "d" ){
+      size_t pos = key.find_first_not_of( "0123456789", mBase.length() );
+      if( pos == string::npos ){
+	return true;
+      }
+    } else if( mParam == "x" ){
+      size_t pos = key.find_first_not_of( "0123456789abcdefABCDEF",
+					  mBase.length() );
+      if( pos == string::npos ){
+	return true;
+      }
+    }
+    return false;
+  }
 };
 
 class ConfigClass
@@ -203,7 +310,28 @@ public:
       newValue.mClass = mCurrentConfig.className();
       mAllConfigs[ newValue.mDesc.base() ] = newValue;
       mCurrentConfig.mValues.push_back( newValue );
+      return true;
     }
+    return false;
+  }
+  
+  bool findValue( const std::string & key, ConfigValue & val )
+  {
+    std::string _key = key;
+    while( _key.length() > 0 ){
+      auto it = mAllConfigs.find( _key );
+      if( it != mAllConfigs.end() ){
+	if( _key != key ){
+	  if (! it->second.isValid( key ) ){
+	    return false;
+	  }
+	}
+	val = it->second;
+	return true;
+      }
+      _key.resize( _key.length() -1 );
+    }
+    return false;
   }
 };
 ConfigSetup buildConfig( const char * config )
@@ -216,7 +344,6 @@ ConfigSetup buildConfig( const char * config )
   return newConfig;
 
 }
-
 struct option config_edit_options[] =
   {
    { "platform", required_argument, nullptr, 'p'},
@@ -226,17 +353,102 @@ struct option config_edit_options[] =
    { "remove",   required_argument, nullptr, 'r'},
    { "comment",  required_argument, nullptr, 'c'},
    { "file",     required_argument, nullptr, 'f'},
+   { "print",    no_argument,       nullptr, 0 },
    { nullptr,    0,                 nullptr, 0 },
   };
 
-using namespace json_lite;
 
-int main( int argc, const char * argv[] )
+bool Section::sectionChange( const std::string & line,
+			     const ConfigSetup & config )
 {
-  test( " ]", false );
-  test( " 12.31234" );
-  test( " \"hello\"" );
-  buildConfig( config );
+  std::string key, value;
+  if( Filter::parseFilter( line, key,value ) ){
+    ConfigValue val;
+    if( config.findValue( key, val ) ){
+      if( val.mClass == "super" ) { // boo special case.  Remove all other filters
+      } else {
+	
+      }
+    }
+  } 
+  return false;
+}
+
+using namespace json_lite;
+void displayConfig( ConfigSetup & setup, const std::string & fileName )
+{
+  std::ifstream file;
+  file.open( fileName );
+  std::string line;
+  while( std::getline(file, line ) ){
+    if( line.length() && line[0] == '[' && line.find( ']' ) != std::string::npos ){
+      printf( "filter %s\n", line.c_str() );
+    } else {
+      printf( "%s\n", line.c_str() );
+    }
+  }
+}
+WholeFile readWholeFile( std::istream & input, ConfigSetup & config)
+{
+  std::string line;
+  WholeFile file;
+  Section currentSection;
+  while( std::getline( input, line ) ){
+    if( line.length() && line[0] == '[' &&
+	line.find( ']' ) != std::string::npos ){
+      file.mSections.push_back( currentSection );
+      currentSection.mLines.clear(); // start a new section
+      currentSection.sectionChange( line, config );
+      printf( "filter %s\n", line.c_str() );
+    } else {
+      currentSection.mLines.push_back( line );
+      printf( "%s\n", line.c_str() );
+    }
+  }
+  return file;
+}
+
+int main( int argc, char * argv[] )
+{
+  std::string file = "/boot/config.txt";
+  bool bInvalid = false;
+  bool bPrintMode = false;
+  while ( bInvalid == false) {
+    int opt_idx = 0;
+    int c = getopt_long( argc, argv, "p:e:a:r:c:f:", config_edit_options, &opt_idx);
+    if( c == -1 ) {
+      break;
+    }
+    switch( c ){
+    case '?':
+      bInvalid = true;
+      break;
+    case 'f':
+      file = optarg;
+      break;
+    case 0:
+    default:
+      {
+	std::string option = config_edit_options[ opt_idx].name;
+	if( option == "print" ){
+	  bPrintMode = true;
+	}
+	printf( "Option %s", config_edit_options[ opt_idx].name );
+	if( optarg ){
+	  printf( " %s", optarg );
+	}
+	printf( "\n" );
+	break;
+      }
+    }
+  }
+  if( bInvalid ){
+    exit( 1 );
+  }
+  ConfigSetup cfg = buildConfig( config );
+  if( bPrintMode ){
+    displayConfig( cfg, file );
+  }
   //  test2( config );
   Description desc( "gpio%d" );
   Description desc2( "0x%x" );
